73c5ccc265b48828424234f5ec1f916acf4dcc31
Caio SBA <caiosba@gmail.com>
2014-12-25 19:40:30 -0300

    Ticket #3266: Adding code coverage and code climate

--- Gemfile
+++ Gemfile
@@ -1,9 +1,11 @@
 source 'https://rubygems.org'
 
-gem 'rails', '4.0.2'
+gem 'rails', '4.0.3'
 gem 'google_drive'
 gem 'sqlite3'
 gem 'xml-simple'
 gem 'embedly'
 gem 'twitter'
 gem 'bower-rails', '~> 0.9.1'
+gem 'simplecov', require: false, group: :test
+gem 'codeclimate-test-reporter', group: :test, require: nil
--- test/controllers/medias_controller_test.rb
+++ test/controllers/medias_controller_test.rb
@@ -1,7 +1,14 @@
-require 'test_helper'
+require File.join(File.expand_path(File.dirname(__FILE__)), '..', 'test_helper')
 
 class MediasControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    super
+    @controller = MediasController.new
+  end
+
+  test "should allow iframe only for embed method" do
+    get :embed
+  end
+
 end
