de088f304eaea813242e01d1b070c25185108256
Caio SBA <caiosba@gmail.com>
2015-04-03 21:53:57 -0300

    Ticket #3622: Bugfix

--- lib/bridge_google_spreadsheet.rb
+++ lib/bridge_google_spreadsheet.rb
@@ -103,8 +103,8 @@ module Bridge
  
       if condition == 'check404'
         Rails.cache.write(cache_key(entry), entry.merge({ oembed: { 'unavailable' => true }}))
-        notify_availability(entry[:index], false)
         update_version
+        notify_availability(entry[:index], false)
       end
     end
   end
