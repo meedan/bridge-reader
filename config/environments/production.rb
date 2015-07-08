8e59c10991b2f0db91357aed94e4419dcee7a2e4
Caio SBA <caiosba@gmail.com>
2015-07-08 19:46:39 -0300

    Ticket #3977: Remove X-Content-Type-Options header

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,7 @@ Bridgembed::Application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.default_headers.delete('X-Content-Type-Options')
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
