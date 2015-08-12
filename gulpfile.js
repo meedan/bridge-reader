01097bdcde93d01eee54c3a449a355c3ee88d569
Chris Blow <cgblow@gmail.com>
2015-08-12 13:12:38 -0700

    new gulpfile with recursive livereloading

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,77 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
-var reload = browserSync.reload;
 var sass = require('gulp-sass');
 var scssFiles = "app/assets/sass/**/*.scss";
 var cssCompileDir = "public/stylesheets";
 var bowerDir = "vendor/assets/bower_components";
 
-// Browsersync configuration
-// 
-// Proxy to the default rails port.
-// Warning: The rails server must be booted to this port before you can run this app.
-var serverConfig = {
-  proxy: "localhost:3000"
-};
+// // A Sass task
+// // 
+// // Run this when any SCSS files change & BrowserSync should auto-update browsers (without a full refresh).
+// gulp.task('sass', function () {
+//   return gulp.src(scssFiles)
+//     .pipe(sass({
+//       includePaths: [bowerDir],
+//       errLogToConsole: true
+//     }))
+//     .pipe(gulp.dest(cssCompileDir))
+//     .pipe(reload({
+//       stream: true
+//     }));
+// });
 
-// A Browsersync task 
-// 
-// Starting the server.
-gulp.task('browser-sync', function () {
-  browserSync(serverConfig);
+// // Another Browsersync task  
+// // 
+// // We "manually" do a "full refresh" of  the browser instead of stream-injection (used in the Sass task)
+// // This is e.g. for when you edit the html
+// // via http://www.browsersync.io/docs/gulp/
+// // 
+// gulp.task('bs-reload', function () {
+//   browserSync.reload();
+// });
+
+// // Default task to be run with `gulp` on CLI
+// gulp.task('default', ['sass', 'browser-sync'], function () {
+//   gulp.watch(scssFiles, ['sass']);
+//   // gulp.watch("public/**/*.html", ['bs-reload']);
+// });
+
+
+/**
+ * Wait for sass, then launch the Server
+ */
+gulp.task('browser-sync', ['sass'], function () {
+  browserSync({
+    server: {
+      proxy: "localhost:3000"
+    }
+  });
 });
 
-// A Sass task
-// 
-// Run this when any SCSS files change & BrowserSync should auto-update browsers (without a full refresh).
 gulp.task('sass', function () {
-  return gulp.src(scssFiles)
+  return gulp.src(['sass/screen.scss', 'sass/screenshot.scss'])
     .pipe(sass({
-      includePaths: [bowerDir],
-      errLogToConsole: true
+      includePaths: ['app/assets/sass'],
+      onError: browserSync.notify
     }))
-    .pipe(gulp.dest(cssCompileDir))
-    .pipe(reload({
+    .pipe(prefix(['last 15 versions', '> 1%'], {
+      cascade: true
+    }))
+    .pipe(gulp.dest('public/stylesheets'))
+    .pipe(browserSync.reload({
       stream: true
-    }));
+    }))
 });
 
-// Another Browsersync task  
-// 
-// We "manually" do a "full refresh" of  the browser instead of stream-injection (used in the Sass task)
-// This is e.g. for when you edit the html
-// via http://www.browsersync.io/docs/gulp/
-// 
-gulp.task('bs-reload', function () {
-  browserSync.reload();
+/**
+ * Watch scss files for changes & recompile
+ */
+gulp.task('watch', function () {
+  gulp.watch('app/assets/sass/*.scss', ['sass']);
 });
 
-// Default task to be run with `gulp` on CLI
-gulp.task('default', ['sass', 'browser-sync'], function () {
-  gulp.watch(scssFiles, ['sass']);
-  // gulp.watch("public/**/*.html", ['bs-reload']);
-});
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
+gulp.task('default', ['browser-sync', 'watch']);
