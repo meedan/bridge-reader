3daa7147b1bbd260d46a801bb1e0d1bdb5009074
Chris Blow <cgblow@gmail.com>
2015-08-12 17:28:17 -0700

    update packages, add tmp.html for testing purposes to work around new proxy bug

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,36 @@
+////
+/// Meedan Gulp configuration 
+/// [1] BrowserSync management w/ Rails proxy
+/// [2] Sass compilation task w/ autoprefixer
+/// [3] Watch task, refresh on all Sass edits
+/// [4] Default task
+
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var prefix = require('gulp-autoprefixer');
 var sass = require('gulp-sass');
-var scssPath = "./app/assets/sass/";
-var scssFiles = scssPath + "**/*.scss";
+var scssPath = "./app/assets/sass";
+var scssFiles = scssPath + "/**/*.scss";
 var cssCompileDir = "public/stylesheets";
 var bowerDir = "vendor/assets/bower_components";
+// var httpProxy = require('http-proxy');
 
+//  var proxy = httpProxy.createProxyServer({
+//    target: 'http://localhost:3000/'
+//  });
+/// [1] 
 gulp.task('browser-sync', ['sass'], function () {
   browserSync({
     server: {
-      proxy: 'localhost:3000',
-      baseDir: 'public/'
+      baseDir: './public',
+      proxy: "localhost:3000"
     }
   });
 });
 
+/// [2] 
 gulp.task('sass', function () {
-  return gulp.src([scssFiles])
+  return gulp.src(scssFiles)
     .pipe(sass({
       includePaths: [scssPath, bowerDir],
       onError: browserSync.notify
@@ -31,8 +44,10 @@ gulp.task('sass', function () {
     }))
 });
 
+/// [3] 
 gulp.task('watch', function () {
   gulp.watch(scssFiles, ['sass']);
 });
 
+/// [4]
 gulp.task('default', ['browser-sync', 'watch']);
