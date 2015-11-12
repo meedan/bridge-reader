475ddf8b78b32ec0d40b4f02349a43f1c55a1c51
Caio SBA <caiosba@gmail.com>
2015-11-11 21:31:10 -0300

    Revert "Ticket #4354: Change title of translation object for HTML view, share on Facebook and share on Twitter"
    This reverts commit b6a06e82cb6f5baa95c8a96c6b8e7661ad91ea5a.

--- test/controllers/medias_controller_test.rb
+++ test/controllers/medias_controller_test.rb
@@ -245,14 +245,14 @@ class MediasControllerTest < ActionController::TestCase
 
   test "should have Facebook metatags for project" do
     get :embed, project: 'google_spreadsheet', format: :html
-    assert_tag(tag: 'meta', attributes: { 'name' => 'og:title', 'content' => 'Translations of Google Spreadsheet' })
+    assert_tag(tag: 'meta', attributes: { 'name' => 'og:title', 'content' => 'Google Spreadsheet' })
     assert_tag(tag: 'meta', attributes: { 'name' => 'og:image', 'content' => /bridge-logo\.png/ })
     assert_tag(tag: 'meta', attributes: { 'property' => 'og:description', 'content' => '' })
   end
 
   test "should have Facebook metatags for collection" do
     get :embed, project: 'google_spreadsheet', collection: 'test', format: :html
-    assert_tag(tag: 'meta', attributes: { 'name' => 'og:title', 'content' => 'Translations of Google Spreadsheet / Test' })
+    assert_tag(tag: 'meta', attributes: { 'name' => 'og:title', 'content' => 'Google Spreadsheet / Test' })
     assert_tag(tag: 'meta', attributes: { 'name' => 'og:image', 'content' => /bridge-logo\.png/ })
     assert_tag(tag: 'meta', attributes: { 'property' => 'og:description', 'content' => 'Translations about: test' })
   end
@@ -260,7 +260,7 @@ class MediasControllerTest < ActionController::TestCase
   test "should have Facebook metatags for item" do
     id = 'cac1af59cc9b410752fcbe3810b36d30ed8e049d'
     get :embed, project: 'google_spreadsheet', collection: 'watchbot', item: id, format: :html
-    assert_tag(tag: 'meta', attributes: { 'name' => 'og:title', 'content' => 'Translation of Caio Almeida: Vídeo do Instagram' })
+    assert_tag(tag: 'meta', attributes: { 'name' => 'og:title', 'content' => 'Google Spreadsheet / Watchbot' })
     assert_tag(tag: 'meta', attributes: { 'name' => 'og:image', 'content' => /#{id}\.png/ })
     assert_tag(tag: 'meta', attributes: { 'property' => 'og:description', 'content' => 'Vídeo do Instagram' })
   end
