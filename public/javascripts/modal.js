c3d17ece68bf162c1620e67647f929ea4d71345d
Daniela Soares Feitosa <danielafeitosa@colivre.coop.br>
2017-01-04 19:36:14 -0300

    Ticket #5476: styling modal

--- public/javascripts/modal.js
+++ public/javascripts/modal.js
@@ -1,15 +1,31 @@
-$( document ).ready(function() {
-     var modal = $('.modal');
-     var span = modal.find('.close');
-     span.click(function() {
-       modal.hide();
-     });
-     window.onclick = function(e) {
-       e.preventDefault();
-         if (e.target.classList == 'modal') {
-            modal.hide();
-         }
-     }
+var itemAsModal = function() {
+  if (Bridge.item != '') {
+    var modal = $('.modal');
+    openModal(modal);
 
-     $(modal).show().appendTo('body');
-});
+    var span = modal.find('.close');
+    span.click(function() {
+      closeModal(modal);
+    });
+    window.onclick = function(e) {
+      e.preventDefault();
+        if (e.target.classList == 'modal') {
+           closeModal(modal);
+        }
+    }
+  }
+};
+
+
+var openModal = function(modal) {
+  $(modal).show().appendTo('body');
+  $('body').addClass('modal-active');
+}
+
+var closeModal = function(modal) {
+  $(modal).hide();
+  $('body').removeClass('modal-active');
+}
+
+
+$(document).ready(itemAsModal);
