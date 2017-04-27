ddbb35af52c5238360b0a8b310598cea6cb49dcf
Daniela Soares Feitosa <danielafeitosa@gmail.com>
2017-04-27 17:19:34 -0300

    Ticket #5651: Added TODO reminder to load config of a project/team

--- lib/check_api_client.rb
+++ lib/check_api_client.rb
@@ -2,9 +2,13 @@ require "graphql/client"
 require "graphql/client/http"
 
 module Check
+
+  # TODO Get config from the team accessed
   @config = BRIDGE_PROJECTS['check-api'].except('type')
+
   @graphql_uri = URI.join(@config['check_api_host'], @config['check_api_graphql']).to_s
   @relay_uri = URI.join(@config['check_api_host'], @config['check_api_relay']).to_s
+
   # TODO Get token from config
   HTTPAdapter = GraphQL::Client::HTTP.new(@graphql_uri) do
     def headers(context)
@@ -17,76 +21,76 @@ module Check
     execute: HTTPAdapter
   )
 
-   TeamQuery = Client.parse <<-'GRAPHQL'
-     query($slug:String!) { team(slug: $slug) { dbid, description, projects { edges { node { title, dbid, description } } } } }
-    GRAPHQL
+  TeamQuery = Client.parse <<-'GRAPHQL'
+    query($slug:String!) { team(slug: $slug) { dbid, description, projects { edges { node { title, dbid, description } } } } }
+  GRAPHQL
 
-   ProjectQuery = Client.parse <<-'GRAPHQL'
-     query($ids:String!) {
-       project(ids: $ids) {
-         dbid
-         title
-         description
-         project_medias {
-           edges {
-             node {
-               dbid
-               url
-               created_at
-               user {
-                 name
-               }
-               language
-               translations_count
-               media {
-                 dbid
-                 quote
-               }
-             }
-           }
-         }
-       }
+  ProjectQuery = Client.parse <<-'GRAPHQL'
+    query($ids:String!) {
+      project(ids: $ids) {
+        dbid
+        title
+        description
+        project_medias {
+          edges {
+            node {
+              dbid
+              url
+              created_at
+              user {
+                name
+              }
+              language
+              translations_count
+              media {
+                dbid
+                quote
+              }
+            }
+          }
+        }
+      }
+   }
+  GRAPHQL
+
+  ProjectMediaQuery = Client.parse <<-'GRAPHQL'
+    query($ids:String!) {
+      project_media(ids: $ids) {
+        dbid
+        url
+        created_at
+        user {
+          name
+        }
+        language
+        translations_count
+        media {
+          dbid
+          quote
+        }
+      }
+   }
+  GRAPHQL
+
+  AnnotationsQuery = Client.parse <<-'GRAPHQL'
+    query {
+      root {
+        annotations {
+          edges {
+            node {
+              dbid
+              annotated_id
+              annotation_type
+              content
+              created_at
+              annotator {
+                name
+              }
+            }
+          }
+        }
+      }
     }
-    GRAPHQL
-
-   ProjectMediaQuery = Client.parse <<-'GRAPHQL'
-     query($ids:String!) {
-       project_media(ids: $ids) {
-         dbid
-         url
-         created_at
-         user {
-           name
-         }
-         language
-         translations_count
-         media {
-           dbid
-           quote
-         }
-       }
-    }
-   GRAPHQL
-
-   AnnotationsQuery = Client.parse <<-'GRAPHQL'
-     query {
-       root {
-         annotations {
-           edges {
-             node {
-               dbid
-               annotated_id
-               annotation_type
-               content
-               created_at
-               annotator {
-                 name
-               }
-             }
-           }
-         }
-       }
-     }
-   GRAPHQL
+  GRAPHQL
 
 end
