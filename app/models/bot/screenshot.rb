45ee01ae5b857c4390f235779b7ab3663e11c80b
Daniela Soares Feitosa <danielafeitosa@gmail.com>
2017-08-28 16:12:14 -0300

    Ticket #6114: Remove code that will now be handled by chromeshot gem
    Also added chromeshot gem on Gemfile and update Gemfile.lock

--- app/models/bot/screenshot.rb
+++
@@ -1,6 +0,0 @@
-class Bot::Screenshot
-  def self.take_screenshot(url, output)
-    fetcher = Chromeshot::Screenshot.new debug_port: 9222
-    fetcher.take_screenshot!(url: url, output: output)
-  end
-end
