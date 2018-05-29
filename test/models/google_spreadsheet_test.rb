87b5083561e58456920a7d53760d40cde1bf83ba
Daniela Soares Feitosa <daniela@meedan.com>
2018-05-29 13:21:04 -0300

    Ticket #6911: Fix test coverage

--- test/models/google_spreadsheet_test.rb
+++ test/models/google_spreadsheet_test.rb
@@ -170,4 +170,17 @@ Not big deal, actually.'
     expected = [{"name"=>"test", "id"=>"test", "project"=>"google_spreadsheet"}, {"name"=>"watchbot", "id"=>"watchbot", "project"=>"google_spreadsheet"}, {"name"=>"first", "id"=>"first", "project"=>"google_spreadsheet"}, {"name"=>"test2", "id"=>"test2", "project"=>"google_spreadsheet"}]
     assert_equal expected, project
   end
+
+  test "handle error when failing authentication on Google Drive" do
+    invalid_credentials = JSON.parse(File.read(BRIDGE_PROJECTS['google_spreadsheet']['google_credentials_path']))
+    invalid_credentials['client_email'] = 'invalid@email.com'
+    File.open('/tmp/invalid.json', "w+") do |f|
+      f.write(invalid_credentials.to_json)
+    end
+    config = {"type"=>"google_spreadsheet", "google_spreadsheet_id"=>"AAAAAAAAA", "google_credentials_path"=>"/tmp/invalid.json"}
+    assert_nothing_raised do
+      Sources::GoogleSpreadsheet.new('google_spreadsheet', config)
+    end
+  end
+
 end
